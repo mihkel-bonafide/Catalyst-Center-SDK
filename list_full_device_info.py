1396a0544a33e1eab9a69da16efda8a867229a4e
Mihkel Gardner <mihkeland@proton.me>
2025-10-15 08:56:14 -0700

    separating function that pulls all device info from the function that pulls specific info

--- list_full_device_info.py
+++ list_full_device_info.py
@@ -2,13 +2,18 @@
 
 from dnacentersdk import api
 from objectify import objectify
+import json
 
 def list_devices():
     object = objectify()
     devices = object.devices.get_device_list()
+    
+    with open('output.json', 'w') as output_file:
+        json.dump(devices, output_file, indent=4)   
+
 
     # for testing/debugging
-    import json; print(json.dumps(devices, indent=4))
+    # import json; print(json.dumps(devices, indent=4))
     # for device in devices["response"]:
     #     print(f"ID: {device['id']}, Management IP: {device['managementIpAddress']}") 
 
